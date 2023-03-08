fed29afef35d09e88740386787688e4cc21d70a5
Chris St John <cbs@primusinterpares.co.uk>
2023-03-08 17:02:05 +0000

    task: hardware timer with sleep power management

--- soft/task.c
+++ soft/task.c
@@ -18,6 +18,31 @@
 
 #include <stdint.h>
 #include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+static volatile uint8_t task_ticks; /**< increments each millisecond */
+
+/**
+ * @brief Calculate the number of F_CPU/256 ticks in 2ms, rounding up
+ */
+#define TIMER_SYSCLK_256_2ms ((F_CPU+64000)/128000)
+
+/**
+ * @brief Timer0 comparison interrupt handler
+ */
+ISR (TIMER0_COMPA_vect)
+{
+    /* Count ticks of timer comparison */
+    TCNT0 = 0;
+
+    /* Modulate comparison to give /128 resolution */
+#if TIMER_SYSCLK_256_2ms % 2
+    OCR0A ^= 1;
+#endif
+
+    task_ticks++;
+}
 
 /**
  * @brief Sleep the processor for a short period
@@ -25,9 +50,13 @@
  */
 static void task_delay(uint8_t milliseconds)
 {
-    /**< TODO: proper low-power sleep */
-    while(milliseconds--)
-        __builtin_avr_delay_cycles(F_CPU/1000);
+    while (task_ticks < milliseconds)
+    {
+        sleep_cpu();
+    }
+
+    /* Reset timer on exit so next delay is aligned to this time */
+    task_ticks = 0;
 }
 
 /**
@@ -38,7 +67,7 @@ static void task_delay(uint8_t milliseconds)
 static uint8_t task_cycle_all(uint8_t ms_later)
 {
     uint8_t all_wake = UINT8_MAX;
-    
+
     /* See TASK_DECLARE() which builds an array of task function pointers */
     extern const task_cycle __task_list_start;
     extern const task_cycle __task_list_end;
@@ -46,10 +75,10 @@ static uint8_t task_cycle_all(uint8_t ms_later)
     {
         /* Array is stored in flash so we need to explicitly read */
         task_cycle task = (task_cycle)pgm_read_word_near(pTask);
-        
+
         /* Give the task a chance to run and tell us how long it can sleep for */
         uint8_t wake = task(ms_later);
-        
+
         /* Collate the soonest waking task */
         if (wake < all_wake)
             all_wake = wake;
@@ -63,15 +92,35 @@ static uint8_t task_cycle_all(uint8_t ms_later)
  */
 int main(void)
 {
+    /* Power management */
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    cli();
+    sleep_enable();
+
+    /* We use TIMER0 with Counter A and /256 prescaler */
+    TCCR0A = 0x02;  /* OCRA */
+    TCCR0B = 0x04;  /* /256 */
+
+    /* Reset counter and set ~1ms compare */
+    TCNT0 = 0;
+    OCR0A = (uint8_t)(TIMER_SYSCLK_256_2ms/2);
+
+    /* Raise interrupt on Compare Match A */
+    TIMSK = 0x10;   /* OCIE0A */
+
     /* Initialise and run */
+    sei();
     for(uint8_t sleep = TASK_STARTUP; sleep != TASK_SHUTDOWN; task_delay(sleep))
     {
         sleep = task_cycle_all(sleep);
         if (sleep == TASK_STARTUP)
             sleep--;
     }
+    cli();
 
     /* Shutdown */
     (void)task_cycle_all(TASK_SHUTDOWN);
-    for(;;);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    cli();
+    sleep_cpu();
 }
